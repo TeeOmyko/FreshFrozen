bf1bafa5dda6dfa502752e15cf6258ae5b15ffd5
Omiko <100170165+TeeOmyko@users.noreply.github.com>
2023-12-20 23:09:22 +0100

    add image folder

---
+++ fooditem.js
@@ -0,0 +1,41 @@
+const foodItem= [
+    {
+    id: 1,
+    name: 'Frozen Chicken',
+    category : 'Chicken',
+    rating : 4.3,
+    price: 13,
+    img: 'images/biryani/Ambur-Chicken-Biryani.jpg',
+    quantity: 1
+},
+{
+    id: 2,
+    name: 'Frozen Fish',
+    category : 'Fish',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/Chicken-Biryani-hyd.jpg',
+    quantity: 1
+},
+{
+    id: 3,
+    name: 'Frozen Turkey',
+    category : 'Turkey',
+    rating : 4.3,
+    price: 18,
+    img: 'images/biryani/egg-biryani.jpeg',
+    quantity: 1
+},
+{
+    id: 4,
+    name: 'Frozen Beef',
+    category : 'Beef',
+    rating : 4.3,
+    price: 15,
+    img: 'images/biryani/goan-fish-biryani.jpg',
+    quantity: 1
+},
+
+]
+
+export {foodItem};
