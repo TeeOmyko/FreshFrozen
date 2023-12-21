c081f0bb18062edf8489b79c4095cc1c5fa8bd69
aishamidris <118014287+aishamidris@users.noreply.github.com>
2023-12-21 12:30:56 +0100

    Index.js

--- Index.js
+++ Index.js
@@ -5,3 +5,8 @@ function displayItems(){
     var beef=  document.getElementById('beef');
     var turkey=  document.getElementById('turkey');
     var fish=  document.getElementById('fish' );}
+
+const chickenData= foodItem.filter((item)=>item.category=='chicken');
+    const beefData= foodItem.filter((item)=>item.category=='beef');
+    const turkeyData= foodItem.filter((item)=>item.category=='turkey');
+    const fishData= foodItem.filter((item)=>item.category=='fish');
