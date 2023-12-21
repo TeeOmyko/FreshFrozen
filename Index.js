d581b71ed0c95a1fba4be14baabc06ae882d7406
aishamidris <118014287+aishamidris@users.noreply.github.com>
2023-12-21 12:18:29 +0100

    Index.js

---
+++ Index.js
@@ -0,0 +1,7 @@
+import {foodItem} from './fooditem.js'
+
+function displayItems(){
+    var chicken= document.getElementById('chicken');
+    var beef=  document.getElementById('beef');
+    var turkey=  document.getElementById('turkey');
+    var fish=  document.getElementById('fish' );}
